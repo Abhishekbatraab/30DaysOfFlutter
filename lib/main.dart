59f30495eb5e30eae755302effe9672f5cdb616b
Abhishek batra <abhishekbatra001@gmail.com>
2021-11-08 17:04:13 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firstapp/pages/home_page.dart';
+import 'package:flutter_firstapp/pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +12,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //data types
+
+    double pi = 3.14;
+    bool isMale = true;
+    num temp = 30.5; //both for int and double
+
+    var day = "Tuesday";
+    const PI = 3.14;
+
     return MaterialApp(
-        home: Material(
-      child: Center(
-        child: Container(
-          child: Text("Welcome to Flutter App"),
-        ),
-      ),
-    ));
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
+    );
   }
 }
